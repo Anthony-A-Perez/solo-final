63219cd68b96c8e98d4fbbb0738bfc04d89d8f52
Anthony-A-Perez <ap.erez@me.com>
2023-07-23 22:40:05 -0700

    update footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,29 @@
 import React from 'react';
-
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Auth from "../utils/auth";
 
 const Footer = () => {
- 
+  const location = useLocation();
+  const navigate = useNavigate();
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   return (
     <footer className="w-100 bg-gray-light-7 pt-3 pb-3 mt-5">
     <div className="container">
       copyright 2023 solo Designs
+      <Link onClick={logout} to="/">Logout</Link>
     </div>
+    {location.pathname !== '/' && (
+          <button
+            className="btn btn-dark mb-3"
+            onClick={() => navigate(-1)}
+          >
+            Go Back
+          </button>
+          
+        )}
   </footer>
   );
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,30 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 
 const Header = () => {
-  const [isLoading, setIsLoading] = useState(true); // Initially set to true to indicate loading
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Initially set to false until token is verified
-
-  useEffect(() => {
-    // Check if there's a token in localStorage
-    const token = localStorage.getItem("id_token");
-
-    // If there's a token and it's not expired, set isLoggedIn to true
-    const checkLoggedIn = async () => {
-      if (token && !Auth.isTokenExpired(token)) {
-        setIsLoggedIn(true);
-      }
-      setIsLoading(false); // Mark the loading process as complete
-    };
-
-    checkLoggedIn();
-  }, []); // The empty dependency array ensures the effect runs only once, on component mount
-
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
-    setIsLoggedIn(false); // Update the isLoggedIn state after logging out
   };
 
   return (
@@ -36,26 +17,25 @@ const Header = () => {
           </Link>
         </div>
         <div>
-          {isLoading ? (
-            // Show a loading indicator or nothing while verifying the token
-            <p>Loading...</p>
-          ) : isLoggedIn ? (
-            // Show the links when isLoggedIn is true
+          {Auth.loggedIn() ? (
             <>
               <ul className="display-f">
                 <li className="ml-1 text-hover-secondary">
-                  <Link to="/posts">Posts</Link>
-                </li>
-                {/* Other links */}
-                <li className="ml-1 text-hover-secondary">
-                  <Link onClick={logout} to="/">
-                    Logout
+                  <Link to="/posts">
+                    Posts
                   </Link>
+                </li>
+                <li className="ml-1 text-hover-secondary">
+                  <Link to="/me">
+                    User
+                  </Link>
+                </li>
+                <li className="ml-1 text-hover-secondary">
+                  <Link onClick={logout} to="/">Logout</Link>
                 </li>
               </ul>
             </>
           ) : (
-            // Show the login and signup links when isLoggedIn is false
             <>
               <ul className="display-f">
                 <li className="ml-1 text-hover-secondary">
