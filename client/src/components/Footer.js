480a42071e99394bb233a9eeadf4ee92bffe5ea9
Anthony-A-Perez <ap.erez@me.com>
2023-07-23 22:52:12 -0700

    footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,14 +10,14 @@ const Footer = () => {
     Auth.logout();
   };
   return (
-    <footer className="w-100 bg-gray-light-7 pt-3 pb-3 mt-5">
+    <footer className="w-100 bg-gray-light-7 pt-3 pb-3 mt-5 display-f">
     <div className="container">
       copyright 2023 solo Designs
-      <Link onClick={logout} to="/">Logout</Link>
+      <Link className="ml-5 text-hover-red" onClick={logout} to="/">Logout</Link>
     </div>
     {location.pathname !== '/' && (
           <button
-            className="btn btn-dark mb-3"
+            className="btn-outlined-orange text-blue text-hover-white mr-5"
             onClick={() => navigate(-1)}
           >
             Go Back
