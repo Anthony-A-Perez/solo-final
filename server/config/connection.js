670f74a70824157cee1d057dd52bc7cda64bb73a
Anthony-A-Perez <ap.erez@me.com>
2023-07-23 18:44:05 -0700

    mongoose connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || process.env.COMPASS);
+mongoose.connect("mongodb+srv://prezanthony:7kBfNjNYgXouH63w@cluster0.awnjqj0.mongodb.net/solo?retryWrites=true&w=majority");
 
 module.exports = mongoose.connection;
